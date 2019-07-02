6c70beefc414e2e9af2d6c649941f1457e8ffbdd
Lindsay Chapin <lnchapin@gmail.com>
2019-07-02 17:27:54 -0600

    fixed products setstate and map

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
 
-const Products =()=>{
-  return(
-    <div>
-      <h2>This is products</h2>
-    </div>
-  );
-};
+class Products extends Component {
+  state={
+    products:[],
+    error: ""
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:8080/api/products")
+      .then(res => res.json())
+      .then(products =>{
+        this.setState({ products: products.products });
+      })
+      .catch(error =>{
+        this.setState({ error });
+      });
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>This is the Products Page</h2>
+        {this.state.error ? <p>Sorry we're having an error: {this.state.error}</p>: ""}
+        {this.state.products.map(product => {
+          return <h2 key={product.id}>{product.name}</h2>
+        })}
+      </div>
+    );
+  }
+}
 
 export default Products;
