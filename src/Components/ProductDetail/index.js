b305cc8a8bd3499aca909e2db0ba0217a0890ffa
Lindsay Chapin <lnchapin@gmail.com>
2019-07-11 19:24:52 -0600

    working on single item page

--- src/Components/ProductDetail/index.js
+++ src/Components/ProductDetail/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import styles from "./products-detail.module.scss";
+
 
 class ProductDetails extends Component{
   state={
@@ -6,13 +8,39 @@ class ProductDetails extends Component{
   }
 
   componentDidMount(){
-    //fetch for 1 product
+    console.log(this.props.match.params.id);
+    fetch(`http://localhost:8080/api/products/${this.props.match.params.id}`)
+      .then(res => res.json())
+      .then(products =>{
+        this.setState({ product: products.products });
+      })
+      .catch(error =>{
+        this.setState({ error });
+      });
   }
 
+
   render(){
-    return(
-      <h2>Product Detail</h2>
-    );
+    if (!this.state.product.id) {
+      return <h2>Loading...</h2>
+    } else {
+      return(
+        <div className={styles.product}>
+          <img src={this.state.product.img_url} alt={this.state.product.name}/>
+          <div>
+            <h2>{this.state.product.name}</h2>
+            <p>{this.state.product.ProductDetail.description}</p>
+            <h2>${(this.state.product.price/100).toFixed(2)}</h2>
+            <p>Category: {this.state.product.Category.name} > {this.state.product.description}</p>
+          </div>
+          <div>
+            <h3>Purchase info</h3>
+            <p>{this.state.product.ProductDetail.quantity} in Stock</p>
+            {this.state.product.ProductDetail.quantity > 0 ? <button>Purchase</button> : <button disabled>Out of Stock</button>}
+          </div>
+        </div>
+      );
+    }
   }
 };
 
